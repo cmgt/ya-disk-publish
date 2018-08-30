cc4e1a86f181e61afacbffeff5ce888ba722c533
cm_gt <porotikovdo@gmail.com>
2018-08-30 11:22:00 +0300

    gitignore

---
+++ VSTSTools/Cmgt.Ya.Disk.Publish/YaDiskPublisher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using YandexDiskNET;
+
+namespace Cmgt.Ya.Disk.Publish
+{
+    public class YaDiskPublisher
+    {
+        private YandexDiskRest yaDiskClient;
+
+        public YaDiskPublisher()
+        {
+            yaDiskClient = new YandexDiskRest("AQAAAAACS5E7AAUtDULTdL3sD0VJqHggFzFIyfw");
+        }
+
+        public void UploadFile(string source, string destination)
+        {
+            var err = yaDiskClient.UploadResource(destination, source, true);            
+        }
+    }
+}
---
+++ VSTSTools/Test.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Cmgt.Ya.Disk.Publish;
+
+namespace Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var yadp = new YaDiskPublisher();
+
+            yadp.UploadFile(@"d:\weather.pdf", "Загрузки/weather.pdf");
+        }
+    }
+}
