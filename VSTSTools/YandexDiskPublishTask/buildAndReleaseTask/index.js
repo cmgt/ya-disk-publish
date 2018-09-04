6c13c57053789514a4d8fc80a237bcdbadf309f7
cm_gt <porotikovdo@gmail.com>
2018-09-04 13:55:00 +0300

    task

--- VSTSTools/YandexDiskPublishTask/buildAndReleaseTask/index.js
+++ VSTSTools/YandexDiskPublishTask/buildAndReleaseTask/index.js
@@ -10,7 +10,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const tl = require("vsts-task-lib/task");
 const path = require("path");
-var YandexDisk = require('yandex-disk').YandexDisk;
+const Q = require("q");
+const yandexDisk = require("yandex-disk");
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
@@ -18,8 +19,7 @@ function run() {
             let contents = tl.getDelimitedInput('contents', '\n', true);
             let sourceFolder = tl.getPathInput('sourcepath', true, true);
             let targetFolder = tl.getPathInput('destpath', true);
-            let oauthToken = tl.getPathInput('oauthtoken', true);
-            let overWrite = tl.getBoolInput('overwrite', false);
+            let oauthToken = tl.getInput('oauthtoken', true);
             // normalize the source folder path. this is important for later in order to accurately
             // determine the relative path of each found file (substring using sourceFolder.length).
             sourceFolder = path.normalize(sourceFolder);
@@ -28,9 +28,10 @@ function run() {
             let matchedFiles = matchedPaths.filter((itemPath) => !tl.stats(itemPath).isDirectory()); // filter-out directories
             // publich the files to the target folder		
             console.log(tl.loc('FoundNFiles', matchedFiles.length));
-            let yd = new YandexDisk(oauthToken);
+            let yd = new yandexDisk.YandexDisk(oauthToken);
             console.log('Create Yandex.Disk web client');
-            matchedFiles.forEach((file) => {
+            let createdFolders = {};
+            matchedFiles.forEach((file) => __awaiter(this, void 0, void 0, function* () {
                 let relativePath = file.substring(sourceFolder.length);
                 // trim leading path separator
                 // note, assumes normalized above
@@ -39,20 +40,60 @@ function run() {
                 }
                 let targetPath = path.join(targetFolder, relativePath);
                 let targetDir = path.dirname(targetPath);
-                let targetFile = path.join(targetFolder, path.basename(relativePath));
-                yd.uploadFile(file, targetFile, function (err) {
-                    console.log(file + ' -> ' + targetFile);
-                    if (err) {
+                if (!createdFolders[targetDir]) {
+                    var created = yield ydCreateDir(yd, targetDir)
+                        .fail((err) => {
                         console.log(err);
-                        throw err;
+                    });
+                    if (created) {
+                        console.log('Success create folder ' + targetDir);
                     }
+                    else {
+                        console.log('Folder ' + targetDir + ' already exist');
+                    }
+                    createdFolders[targetDir] = true;
+                }
+                console.log(file + ' -> ' + targetPath);
+                yield ydUploadFile(yd, file, targetPath)
+                    .fail((err) => {
+                    console.log(err);
                 });
-            });
+                ;
+                console.log('Success upload file ' + file);
+            }));
         }
         catch (err) {
             tl.setResult(tl.TaskResult.Failed, err.message);
         }
     });
+}
+function ydCreateDir(yd, path) {
+    const deferred = Q.defer();
+    tl.debug('call yd.mkdir');
+    yd.mkdir(path, (err, created) => {
+        if (err) {
+            console.log('Fail create folder ' + path);
+            deferred.reject(err);
+        }
+        else {
+            deferred.resolve(created);
+        }
+    });
+    return deferred.promise;
+}
+function ydUploadFile(yd, file, targetPath) {
+    const deferred = Q.defer();
+    tl.debug('call yd.uploadFile');
+    yd.uploadFile(file, targetPath, (err) => {
+        if (err) {
+            console.log('Fail upload file ' + file + ' to target path ' + targetPath);
+            deferred.reject(err);
+        }
+        else {
+            deferred.resolve();
+        }
+    });
+    return deferred.promise;
 }
 run();
 //# sourceMappingURL=index.js.map
--- VSTSTools/YandexDiskPublishTask/buildAndReleaseTask/index.ts
+++ VSTSTools/YandexDiskPublishTask/buildAndReleaseTask/index.ts
@@ -1,17 +1,17 @@
-import tl = require('vsts-task-lib/task');
-import fs = require('fs');
-import path = require('path');
-var YandexDisk = require('yandex-disk').YandexDisk;
+import tl = require("vsts-task-lib/task");
+import path = require("path");
+import Q = require("q");
+import yandexDisk = require("yandex-disk");
 
 async function run() {
     try {        
-		tl.setResourcePath(path.join(__dirname, 'task.json'));
+
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
 		
 		let contents: string[] = tl.getDelimitedInput('contents', '\n', true);
 		let sourceFolder: string = tl.getPathInput('sourcepath', true, true);
 		let targetFolder: string = tl.getPathInput('destpath', true);		
-		let oauthToken: string = tl.getPathInput('oauthtoken', true);		
-		let overWrite: boolean = tl.getBoolInput('overwrite', false);
+		let oauthToken: string = tl.getInput('oauthtoken', true);
 		
 		// normalize the source folder path. this is important for later in order to accurately
 		// determine the relative path of each found file (substring using sourceFolder.length).
@@ -23,11 +23,13 @@ async function run() {
 		
 		// publich the files to the target folder		
 		console.log(tl.loc('FoundNFiles', matchedFiles.length));		
-		
-		let yd = new YandexDisk(oauthToken);
+		               
+        let yd = new yandexDisk.YandexDisk(oauthToken);
 		console.log('Create Yandex.Disk web client');
 		
-		matchedFiles.forEach((file: string) => {            		
+		let createdFolders: { [folder: string]: boolean } = {};
+		
+		matchedFiles.forEach(async (file: string) => {            		
             let relativePath = file.substring(sourceFolder.length);
 			// trim leading path separator
 			// note, assumes normalized above
@@ -37,20 +39,69 @@ async function run() {
 
             let targetPath = path.join(targetFolder, relativePath);
             let targetDir = path.dirname(targetPath);
-			let targetFile = path.join(targetFolder, path.basename(relativePath));
-						
-			yd.uploadFile(file, targetFile, function(err) {
-				console.log(file + ' -> ' + targetFile);
-				if (err) {					
-					console.log(err);
-					throw err;
-				}
-			});                                             
-		});	
+
+		    if (!createdFolders[targetDir]) {
+		        var created = await ydCreateDir(yd, targetDir)
+                    .fail((err) => {
+		                console.log(err);
+		            });
+
+		        if (created) {
+		            console.log('Success create folder ' + targetDir);
+		        } else {
+		            console.log('Folder ' + targetDir + ' already exist');
+		        }
+
+		        createdFolders[targetDir] = true;
+		    }
+
+		    console.log(file + ' -> ' + targetPath);
+		    await ydUploadFile(yd, file, targetPath)
+		        .fail((err) => {
+		            console.log(err);
+		        });;
+		    console.log('Success upload file ' + file);
+		});        
     }
     catch (err) {
         tl.setResult(tl.TaskResult.Failed, err.message);
     }
+}
+
+function ydCreateDir(yd: yandexDisk.YandexDisk, path: string): Q.Promise<boolean> {
+    const deferred = Q.defer<boolean>();
+
+    tl.debug('call yd.mkdir');
+
+    yd.mkdir(path,
+        (err, created) => {
+            if (err) {
+                console.log('Fail create folder ' + path);
+                deferred.reject(err);
+            } else {
+                deferred.resolve(created);
+            }
+        });       
+
+    return deferred.promise;
+}
+
+function ydUploadFile(yd: yandexDisk.YandexDisk, file: string, targetPath: string): Q.Promise<void> {
+    const deferred = Q.defer<void>();
+
+    tl.debug('call yd.uploadFile');
+
+    yd.uploadFile(file, targetPath, 
+        (err) => {
+            if (err) {
+                console.log('Fail upload file ' + file + ' to target path ' + targetPath);
+                deferred.reject(err);
+            } else {
+                deferred.resolve();
+            }
+        });       
+
+    return deferred.promise;
 }
 
 run();
